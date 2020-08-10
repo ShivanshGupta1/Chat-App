d12dfa121d6c1c57de63f56a03d4ea941c5d8797
Shivansh Gupta <mail@shivanshgupta.com>
2020-08-10 17:35:25 +0530

    Initial Release Of Chat App!

---
+++ server.js
@@ -0,0 +1,51 @@
+// const express = require('express')
+// const app = express()
+// const http = require('http')
+// const server = http.createServer(app)
+// const io = require('socket.io')(server)
+
+// app.get('/', function(req,res){
+//     res.sendFile(__dirname+'/index.html')
+// })
+// io.on('connection' , (socket)=> {
+//     console.log('Socket.io is connected', 'User Id - ', socket.id)
+//     io.on('Chat Data', (data)=>{
+//         console.log(data)
+//         io.sockets.emit('Chat Data', data)
+//     })
+
+// })
+// server.listen(2000, ()=>{
+//   console.log('Running...')
+// })
+let express = require('express');
+let socket = require('socket.io');
+let app = express();
+let server = app.listen(2000, () => {
+    console.log("server listening")
+})
+
+//static files
+
+app.use(express.static('Public'))
+
+
+// socket setup
+
+let io = socket(server);
+
+
+io.on('connection', function (socket) {
+    console.log("socket is on connection", socket.id);
+
+
+    socket.on('Chat Data', function (data) {
+        io.sockets.emit('Chat Data', data);
+    })
+
+    //     socket.on('someone typing',function(data){
+    // })
+
+})
+
+
